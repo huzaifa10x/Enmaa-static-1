d5caf005edc28bb5299d14ae39abdabbd6b6176d
Huzaifa <huzaifa.10xdigital@gmail.com>
2026-04-21 16:40:24 +0500

    dsf

--- _next/static/Oo-AfhsWB7ihcVzOzqp4R/_buildManifest.js
+++ _next/static/Oo-AfhsWB7ihcVzOzqp4R/_buildManifest.js
@@ -1,15 +1,10 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/e8c313c7d986a363.js"
+    "./static/chunks/c85b853487797545.js"
   ],
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/..//_next/:path+",
-        "destination": "/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
